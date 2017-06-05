76bcf2e36817fb7cedac7588ce5c3f9c304fd93b
Gregory LeMasurier <gregorylemasurier@gmail.com>
2017-06-05 14:45:44 -0400

    Working!

--- src/my_publisher_robot_node.cpp
+++ src/my_publisher_robot_node.cpp
@@ -3,16 +3,29 @@
 #include "sensor_msgs/LaserScan.h"
 
 ros::Publisher pub;
+sensor_msgs::LaserScan laser_msg;
 
 void LaserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
-//geometry_msgs::Twist twist_msg;
-//pub.publish(twist_msg);
+laser_msg = *msg;
 }
 
-void TwistCallback(const geometry_msgs::Twist::ConstPtr& msg)
+void JoystickCallback(const geometry_msgs::Twist::ConstPtr& msg)
 {
-pub.publish(msg);
+int size = ((laser_msg.angle_max - laser_msg.angle_min)/laser_msg.angle_increment) - 1;
+int index = 0;
+geometry_msgs::Twist twist_msgs;
+
+twist_msgs = *msg;
+
+for(index = (size/4); index < ((size*3)/4); index++){
+if(laser_msg.ranges[index] < 0.3 && msg->linear.x > 0){
+twist_msgs.linear.x = 0;
+twist_msgs.angular.z = 0;
+}
+}
+
+pub.publish(twist_msgs);
 }
 
 int main(int argc, char* argv[]){
@@ -21,9 +34,9 @@ ros::init(argc, argv, "my_publisher_robot");
 
 ros::NodeHandle n;
 
-pub = n.advertise<geometry_msgs::Twist>("/Robot/cmd_vel",1000);
+pub = n.advertise<geometry_msgs::Twist>("/cmd_vel",1000);
 
-ros::Subscriber subTwist = n.subscribe("/cmd_vel", 1000, TwistCallback);
+ros::Subscriber subTwist = n.subscribe("/robot_joystick", 1000, JoystickCallback);
 ros::Subscriber subLaser = n.subscribe("/base_scan", 1000, LaserCallback);
 
 ros::spin();
